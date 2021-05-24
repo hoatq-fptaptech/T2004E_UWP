09806f66c5669d1afb1babd8697ca2a092b6dd44
hoatq-fptaptech <hoatq4@fpt.edu.vn>
2021-05-24 20:04:21 +0700

    session 2

---
+++ UWP/Models/User.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UWP.Models
+{
+     class User
+    {
+        private string name;
+        private string email;
+
+        public User(string name,string email)
+        {
+            Name = name;
+            Email = email;
+        }
+
+        public string Name // properties
+        {
+            get => name;
+            set => name = value;
+        }
+
+        public string Email
+        {
+            get => email;
+            set => email = value;
+        }
+    }
+}
---
+++ UWP/Pages/SplitViewDemo.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using UWP.Models;
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace UWP.Pages
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class SplitViewDemo : Page
+    {
+        public SplitViewDemo()
+        {
+            this.InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            SV.IsPaneOpen = !SV.IsPaneOpen;
+        }
+
+        private void ListUsers_Loaded(object sender, RoutedEventArgs e)
+        {
+            User u1 = new User("Nguyễn Văn An", "annv@gmail.com");
+            User u2 = new User("Lê Văn Đức", "duclv@gmail.com");
+            ListUsers.Items.Add(u1);
+            ListUsers.Items.Add(u2);
+        }
+    }
+}
